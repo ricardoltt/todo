6ef75ad346e6b6d7224f921f430a953f91ac49f3
Ricardo Temoteo <ricardo.ltemoteo@gmail.com>
2022-04-22 23:31:36 -0300

    fix: just forcing commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,11 @@
-import { render } from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom'
 
 import { App } from './App'
 
-render(<App />, document.getElementById('root'))
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
